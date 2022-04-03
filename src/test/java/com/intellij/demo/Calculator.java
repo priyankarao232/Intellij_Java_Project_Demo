62e7a91d0d6c060ce1f8b31d887afd753fce3106
test <priyankarao232@gmail.com>
2022-04-02 18:54:24 -0700

    add print statement for sunstract

--- src/test/java/com/intellij/demo/Calculator.java
+++ src/test/java/com/intellij/demo/Calculator.java
@@ -5,7 +5,10 @@ public class Calculator {
         int a=10;
         int b=6;
         int add = add(a,b);
+        int sub= a-b;
         System.out.format("Add %d + %d = %d",a,b,add);
+        System.out.format("Add %d + %d = %d",a,b,sub);
+
     }
 
     public static int add(int a, int b){
@@ -14,5 +17,7 @@ public class Calculator {
     public static int substract(int a, int b){
         return a-b;
     }
+
+
 
 }
