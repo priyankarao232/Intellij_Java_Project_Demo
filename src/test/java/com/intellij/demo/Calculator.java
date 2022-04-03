943258fd613c75e44ed84ede470964865f283443
test <priyankarao232@gmail.com>
2022-04-03 09:16:53 -0700

    added multiply method

--- src/test/java/com/intellij/demo/Calculator.java
+++ src/test/java/com/intellij/demo/Calculator.java
@@ -5,9 +5,11 @@ public class Calculator {
         int a=10;
         int b=6;
         int add = add(a,b);
-        int sub= a-b;
+        int sub= substract(a, b);
+        int mul=multiply(a,b);
         System.out.format("Add %d + %d = %d",a,b,add);
         System.out.format("Add %d + %d = %d",a,b,sub);
+        System.out.format("Mul %d * %d = %d",a,b,mul);
 
     }
 
@@ -16,6 +18,9 @@ public class Calculator {
     }
     public static int substract(int a, int b){
         return a-b;
+    }
+    public static int multiply(int a, int b){
+        return a*b;
     }
 
 
