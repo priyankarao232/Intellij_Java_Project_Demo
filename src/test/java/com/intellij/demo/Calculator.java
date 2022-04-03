2028581513508883f8e9227fb164a24740312c75
test <priyankarao232@gmail.com>
2022-04-03 11:24:09 -0700

    add modulus method

--- src/test/java/com/intellij/demo/Calculator.java
+++ src/test/java/com/intellij/demo/Calculator.java
@@ -7,10 +7,11 @@ public class Calculator {
         int add = add(a,b);
         int sub= substract(a, b);
         int mul=multiply(a,b);
+        int mod= modulus(a,b);
         System.out.format("Add %d + %d = %d",a,b,add);
         System.out.format("Add %d + %d = %d",a,b,sub);
         System.out.format("Mul %d * %d = %d",a,b,mul);
-
+        System.out.printf("Mod %d %% %d = %d",a,b,mod);
     }
 
     public static int add(int a, int b){
@@ -23,6 +24,8 @@ public class Calculator {
         return a*b;
     }
 
-
+    public static int modulus(int a, int b){
+        return a%b;
+    }
 
 }
