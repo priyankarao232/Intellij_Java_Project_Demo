52d133f182e7c27569f88062f3b24285706000a9
test <priyankarao232@gmail.com>
2022-04-02 18:43:56 -0700

    updated calculator source file and ad.gitignore

--- src/test/java/com/intellij/demo/Calculator.java
+++ src/test/java/com/intellij/demo/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
         int a=10;
         int b=6;
         int add = add(a,b);
+        System.out.format("Add %d + %d = %d",a,b,add);
     }
 
     public static int add(int a, int b){
