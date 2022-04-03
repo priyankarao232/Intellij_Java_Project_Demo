709442ba9604c61bb05a144a13df35fe9c19c50d
test <priyankarao232@gmail.com>
2022-04-02 18:12:21 -0700

    initial commit to local repository

---
+++ src/test/java/com/intellij/demo/Calculator.java
@@ -0,0 +1,13 @@
+package com.intellij.demo;
+
+public class Calculator {
+    public static void main(String[] args){
+        int a=10;
+        int b=6;
+        int add = add(a,b);
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
